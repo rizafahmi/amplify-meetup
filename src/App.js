73942c9f6fd0ca3f74b2c5fbf0906592a65cedac
Riza Fahmi <rizafahmi@gmail.com>
2020-06-18 13:04:36 +0700

    Basic design Daftar Acara

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Daftar Acara</h1>
+      <div className="nes-container">
+        <div className="nes-container with-title">
+          <h3 className="title">JakartaJS</h3>
+          <div className="event">
+            <div className="dates">
+              <span className="date">19</span>
+              <span className="month">Maret</span>
+              <span className="year">2020</span>
+            </div>
+            <div className="event-detail">
+              <h4>Doing awesome stuff with JavaScript</h4>
+              <div className="location">📍 Jakarta</div>
+            </div>
+            <div className="cta">
+              <a href="#" className="nes-btn is-primary">
+                detail
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
