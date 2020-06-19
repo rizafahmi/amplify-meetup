91d19dce9add1e93761b620616892fdbdf50d698
Riza Fahmi <rizafahmi@gmail.com>
2020-06-20 05:45:06 +0700

    Change css framework

--- src/App.js
+++ src/App.js
@@ -36,65 +36,56 @@ function App() {
     })();
   });
   return (
-    <div className="container">
-      <div className="nes-container">
+    <>
+      <header>
+        <h1>Amplify Meetup</h1>
         <AmplifySignOut />
-      </div>
-      <h1 className="title">Daftar Acara</h1>
-      <div className="nes-container with-title">
-        <h3 className="title">Acara Baru</h3>
-        <form onSubmit={handleFormSubmit}>
-          <div
-            className="nes-field is-inline"
-            style={{ marginBottom: '1.5rem' }}
-          >
-            <label htmlFor="community">Nama Komunitas</label>
-            <input
-              type="text"
-              name="community"
-              className="nes-input"
-              onChange={handleCommunityChange}
-            />
-          </div>
+      </header>
+      <main>
+        <article>
+          <aside>
+            <form onSubmit={handleFormSubmit}>
+              <fieldset>
+                <h3>Acara Baru</h3>
+                <label htmlFor="community">Nama Komunitas</label>
+                <input
+                  type="text"
+                  name="community"
+                  onChange={handleCommunityChange}
+                />
 
-          <div className="nes-field is-inline">
-            <label htmlFor="title">Judul Acara</label>
-            <input
-              type="text"
-              name="title"
-              className="nes-input"
-              onChange={handleTitleChange}
-            />
-          </div>
-          <button type="submit" className="nes-btn is-success">
-            Simpan
-          </button>
-        </form>
-      </div>
-      <div className="nes-container">
-        {meetups.map((meetup, index) => (
-          <div className="nes-container with-title" key={index}>
-            <h3 className="title">{meetup.community}</h3>
-            <div className="event">
-              <div className="dates">
-                <span className="date">19</span>
-                <span className="month">Maret</span>
-                <span className="year">2020</span>
-              </div>
-              <div className="event-detail">
-                <h4>{meetup.title}</h4>
-                <div className="location">📍 Jakarta</div>
-              </div>
-              <div className="cta">
-                <a href="#" className="nes-btn is-primary">
-                  detail
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+                <label htmlFor="title">Judul Acara</label>
+                <input type="text" name="title" onChange={handleTitleChange} />
+                <button type="submit">Simpan</button>
+              </fieldset>
+            </form>
+          </aside>
+          <section>
+            {meetups.map((meetup, index) => (
+              <fieldset key={index}>
+                <h3 className="title">{meetup.community}</h3>
+                <div className="event">
+                  <div className="dates">
+                    <span className="date">19</span>
+                    <span className="month">Maret</span>
+                    <span className="year">2020</span>
+                  </div>
+                  <div className="event-detail">
+                    <h4>{meetup.title}</h4>
+                    <div className="location">📍 Jakarta</div>
+                  </div>
+                  <div className="cta">
+                    <a href="#" className="nes-btn is-primary">
+                      <button>detail</button>
+                    </a>
+                  </div>
+                </div>
+              </fieldset>
+            ))}
+          </section>
+        </article>
+      </main>
+    </>
   );
 }
 
